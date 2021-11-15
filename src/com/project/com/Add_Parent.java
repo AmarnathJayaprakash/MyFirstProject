10e332805e52921c95ca075093504dfa541671b1
AmarnathJayaprakash <94186524+AmarnathJayaprakash@users.noreply.github.com>
2021-11-15 14:01:06 +0530

    Update Add_Parent.java
    I just remove the unwanted spaces in my project

--- src/com/project/com/Add_Parent.java
+++ src/com/project/com/Add_Parent.java
@@ -1,14 +1,9 @@
 package com.project.com;
 
 public class Add_Parent {
-	
-	
-	public int addMethod(int a , int b) {
-      int c = a+b;
-      return c;
-	}
-	
-	
-	
 
+public int addMethod(int a , int b) {
+    int c = a+b;
+    return c;
+}
 }
